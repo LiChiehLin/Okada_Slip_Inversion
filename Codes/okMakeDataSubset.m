ec1ebb16dd8d4173ebb562d49ff0615585715204
Li-Chieh J. Lin <llin148@ucr.edu>
2025-12-18 16:22:58 -0800

    Fix data cropping logic in okMakeDataSubset.m
    Updated cropping logic to handle cases where data was not read in, preventing issues with subset creation.

--- Codes/okMakeDataSubset.m
+++ Codes/okMakeDataSubset.m
@@ -10,6 +10,10 @@
 %             ***           okMakeDataSubset.m            ***             %
 %             ***********************************************             %
 %                                                                         %
+% (Update: 2025.12.17)                                                    %
+% Update on cropping data when the data was not read in. Prevents this    %
+% routine from not working properly                                       %
+%                                                                         %
 % Make subset of the input data structure from okLoadData.m               %
 % Cropping the data to make it smaller                                    %
 %                                                                         %
@@ -38,40 +42,70 @@ disp(strcat('*** Orig size:',32,num2str(Row),32,num2str(Col)))
 disp('**************************************')
 DataSubset = DataStruct;
 if any(strcmp(fieldnames(DataStruct.(Dataset)),'Displ'))
-    Displ = DataStruct.(Dataset).Displ;
-    DisplSub = Displ(rmin:rmax,cmin:cmax);
-    disp('*** Making subset of Displacement')
-    DataSubset.Subset.Displ = DisplSub;
+    [r,c] = size(DataStruct.(Dataset).Displ);
+    if (r > 1) || (c > 1)
+        Displ = DataStruct.(Dataset).Displ;
+        DisplSub = Displ(rmin:rmax,cmin:cmax);
+        disp('*** Making subset of Displacement')
+        DataSubset.Subset.Displ = DisplSub;
+    else
+        disp('*** Skipping making subset of Displacement')
+    end
 end
 if any(strcmp(fieldnames(DataStruct.(Dataset)),'Azimuth'))
-    Azimuth = DataStruct.(Dataset).Azimuth;
-    AzimuthSub = Azimuth(rmin:rmax,cmin:cmax);
-    disp('*** Making subset of Azimuth')
-    DataSubset.Subset.Azimuth = AzimuthSub;
+    [r,c] = size(DataStruct.(Dataset).Azimuth);
+    if (r > 1) || (c > 1)
+        Azimuth = DataStruct.(Dataset).Azimuth;
+        AzimuthSub = Azimuth(rmin:rmax,cmin:cmax);
+        disp('*** Making subset of Azimuth')
+        DataSubset.Subset.Azimuth = AzimuthSub;
+    else
+        disp('*** Skipping making subset of Azimuth')
+    end
 end
 if any(strcmp(fieldnames(DataStruct.(Dataset)),'Incidence'))
-    Incidence = DataStruct.(Dataset).Incidence;
-    IncidenceSub = Incidence(rmin:rmax,cmin:cmax);
-    disp('*** Making subset of Incidence')
-    DataSubset.Subset.Incidence = IncidenceSub;
+    [r,c] = size(DataStruct.(Dataset).Incidence);
+    if (r > 1) || (c > 1)
+        Incidence = DataStruct.(Dataset).Incidence;
+        IncidenceSub = Incidence(rmin:rmax,cmin:cmax);
+        disp('*** Making subset of Incidence')
+        DataSubset.Subset.Incidence = IncidenceSub;
+    else
+        disp('*** Skipping making subset of Incidence')
+    end
 end
 if any(strcmp(fieldnames(DataStruct.(Dataset)),'Longitude'))
-    Longitude = DataStruct.(Dataset).Longitude;
-    LongitudeSub = Longitude(rmin:rmax,cmin:cmax);
-    disp('*** Making subset of Longitude')
-    DataSubset.Subset.Longitude = LongitudeSub;
+    [r,c] = size(DataStruct.(Dataset).Longitude);
+    if (r > 1) || (c > 1)
+        Longitude = DataStruct.(Dataset).Longitude;
+        LongitudeSub = Longitude(rmin:rmax,cmin:cmax);
+        disp('*** Making subset of Longitude')
+        DataSubset.Subset.Longitude = LongitudeSub;
+    else
+        disp('*** Skipping making subset of Longitude')
+    end
 end
 if any(strcmp(fieldnames(DataStruct.(Dataset)),'Latitude'))
-    Latitude = DataStruct.(Dataset).Latitude;
-    LatitudeSub = Latitude(rmin:rmax,cmin:cmax);
-    disp('*** Making subset of Latitude')
-    DataSubset.Subset.Latitude = LatitudeSub;
+    [r,c] = size(DataStruct.(Dataset).Latitude);
+    if (r > 1) || (c > 1)
+        Latitude = DataStruct.(Dataset).Latitude;
+        LatitudeSub = Latitude(rmin:rmax,cmin:cmax);
+        disp('*** Making subset of Latitude')
+        DataSubset.Subset.Latitude = LatitudeSub;
+    else
+        disp('*** Skipping making subset of Latitude')
+    end
 end
 if any(strcmp(fieldnames(DataStruct.(Dataset)),'Geometry'))
-    Geometry = DataStruct.(Dataset).Geometry;
-    GeometrySub = Geometry(rmin:rmax,cmin:cmax);
-    disp('*** Making subset of Geometry')
-    DataSubset.Subset.Geometry = GeometrySub;
+    [r,c] = size(DataStruct.(Dataset).Geometry);
+    if (r > 1) || (c > 1)
+        Geometry = DataStruct.(Dataset).Geometry;
+        GeometrySub = Geometry(rmin:rmax,cmin:cmax);
+        disp('*** Making subset of Geometry')
+        DataSubset.Subset.Geometry = GeometrySub;
+    else
+        disp('*** Skipping making subset of Geometry')
+    end
 end
 
 % Store the subset index boundaries
@@ -82,5 +116,4 @@ disp('**************************************')
 disp(strcat('Subset size:',32,num2str(rmax-rmin+1),32,num2str(cmax-cmin+1)))
 disp(' ')
 end
-
 
